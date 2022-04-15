ea1c7e07d5fe7ad84cd788b15fa7e452752fac20
linn-holmqvist-nti-johanneberg <74715034+linn-holmqvist-nti-johanneberg@users.noreply.github.com>
2022-04-15 02:01:11 +0200

    sent på natten 15/4

--- app.rb
+++ app.rb
@@ -17,13 +17,27 @@ get('/laws') do
   slim(:"laws/index", locals:{the_laws:result})
 end
 
+get('/laws/new') do
+  slim(:"laws/new")
+end
+
+post('/laws/new') do
+  law_name = params[:law_name]
+  description = params[:description]
+  district = params[:district]
+  access_number = params[:access_number].to_i
+  db = SQLite3::Database.new('db/laws.db')
+  db.execute("INSERT INTO laws (law_name, description) VALUES (?,?)", law_name, description)
+  db.execute("INSERT INTO district (district_name, access) VALUES (?,?)", district, access_number)
+  redirect('/laws')
+end
+
 get('/laws/:id') do 
   id = params[:id].to_i
   db = SQLite3::Database.new('db/laws.db')
   db.results_as_hash = true
   result = db.execute("SELECT * FROM laws WHERE law_id = ?", id).first
   result2 = db.execute("SELECT district_name FROM district WHERE district_id IN (SELECT district_id FROM law_district_relation WHERE law_id = ?)", id).first
-  p result2
   slim(:"laws/show", locals:{result:result,result2:result2})
 end
 
